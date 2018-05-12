dd89916b8cb75e7c303d7bef1fdb72b45fd8772c
Michael Kang <michaelkang85@gmail.com>
2018-05-12 18:40:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -38,7 +38,7 @@ class App < Sinatra::Base
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
   @string = [params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]].join(' ')
-  "#{@string}"
+  "#{@string}."
   end
 #
 get '/:operation/:number1/:number2' do
