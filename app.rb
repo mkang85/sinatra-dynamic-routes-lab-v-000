377c3b264766a8d2f56afa5b5c9d5711a500e24b
Michael Kang <michaelkang85@gmail.com>
2018-05-12 18:22:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,14 +12,14 @@ class App < Sinatra::Base
     "#{@square}"
   end
 
-  get '/say/:number/:phrase' do
-    @number = params[:number].to_i
-    @phrase = params[:phrase]
-    i = 0;
-    while i < @number
-    puts @phrase
-    i++
-  end
+  # get '/say/:number/:phrase' do
+  #   @number = params[:number].to_i
+  #   @phrase = params[:phrase]
+  #   i = 0;
+  #   while i < @number
+  #   puts @phrase
+  #   i++
+  # end
 
   get '/say/:word1/:word2/:word3/:word4/:word5'
   @string = [params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]].join(' ') 
